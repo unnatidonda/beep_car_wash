4e30081d0c51c8d3d592a5a4a46cb0a9bec4335f
Unnati Donda <134287519+unnatidonda@users.noreply.github.com>
2023-08-02 10:41:21 +0530

    new_screen

--- lib/beep_car_wash_screen/splash_screen.dart
+++ lib/beep_car_wash_screen/splash_screen.dart
@@ -10,6 +10,10 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
   }
 }
