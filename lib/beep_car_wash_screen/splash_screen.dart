c7b6272547eedce9d1bf04dc5b38b618ec3d63db
Unnati Donda <134287519+unnatidonda@users.noreply.github.com>
2023-08-04 17:41:49 +0530

    changes

--- lib/beep_car_wash_screen/splash_screen.dart
+++ lib/beep_car_wash_screen/splash_screen.dart
@@ -10,6 +10,11 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    double screenHeight = size.height;
+    double screenWidth = size.width;
+    EdgeInsets devicePadding = MediaQuery.of(context).viewPadding;
+
     return Scaffold(
       body: Column(
         children: [],
