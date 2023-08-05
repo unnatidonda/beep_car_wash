02fd3a54b1aa758b40ee55c3ba21d0b5bb9f54ce
Unnati Donda <134287519+unnatidonda@users.noreply.github.com>
2023-08-05 18:57:10 +0530

    changes

--- lib/view/splash/splash_screen.dart
+++ lib/view/splash/splash_screen.dart
@@ -7,6 +7,7 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
+
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
