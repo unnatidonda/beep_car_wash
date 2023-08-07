920a4688cafcc9cf282c810536014293e418606c
Unnati Donda <134287519+unnatidonda@users.noreply.github.com>
2023-08-07 11:53:47 +0530

    changes

--- lib/view/onboarding_screen.dart
+++ lib/view/onboarding_screen.dart
@@ -15,14 +15,67 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     double screenWidth = size.width;
     EdgeInsets devicePadding = MediaQuery.of(context).viewPadding;
     return Scaffold(
-      body: Column(
-        children: [
-          Image.asset(
-            "assets/images/wash_your_car.png",
-            height: screenWidth / 1.1,
-            width: screenWidth / 0.0,
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              "assets/images/wash_your_car.png",
+              height: screenWidth / 0.6,
+              width: screenWidth / 0.5,
+            ),
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                textAlign: TextAlign.start,
+                "Wash Your Car",
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontFamily: "Poppins",
+                  fontSize: 28,
+                  color: Color(0xFF000000),
+                ),
+              ),
+            ),
+            const Text(
+              textAlign: TextAlign.start,
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, \nsed do eiusmod tempor incididunt ut labore ...",
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontFamily: "Poppins",
+                fontSize: 15,
+                color: Color(0xFF898989),
+              ),
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: const MaterialStatePropertyAll(Color(0xFF00AB6C)),
+                shape: MaterialStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.circular(5))),
+                // fixedSize: MaterialStatePropertyAll(
+                // Size(width: 10, height!),
+                // ),
+              ),
+              // onPressed: onPress ?? () {},
+              onPressed: () {},
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Get Started",
+                    style: TextStyle(
+                      fontSize: 17,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: "Poppins",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
