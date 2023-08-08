2551d3fba10c1fdd23d397e3c9454f8d59f53c52
Unnati Donda <134287519+unnatidonda@users.noreply.github.com>
2023-08-08 13:41:49 +0530

    time picker screen

---
+++ lib/task_screen/time_picker_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TimePickerScreen extends StatefulWidget {
+  const TimePickerScreen({super.key});
+
+  @override
+  State<TimePickerScreen> createState() => _TimePickerScreenState();
+}
+
+class _TimePickerScreenState extends State<TimePickerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
