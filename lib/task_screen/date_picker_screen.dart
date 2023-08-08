9e5bdf5c677374d58e02bbdb58b725d63f45a359
Unnati Donda <134287519+unnatidonda@users.noreply.github.com>
2023-08-08 13:48:36 +0530

    time picker screen

--- lib/task_screen/date_picker_screen.dart
+++ lib/task_screen/date_picker_screen.dart
@@ -8,6 +8,25 @@ class DatePickerScreen extends StatefulWidget {
 }
 
 class _DatePickerScreenState extends State<DatePickerScreen> {
+  // DateTime? pickedDate = await showDatePicker(
+  // context: context,
+  // initialDate: DateTime.now(),
+  // firstDate: DateTime(1950),
+  // //DateTime.now() - not to allow to choose before today.
+  // lastDate: DateTime(2100));
+  //
+  // if (pickedDate != null) {
+  // print(
+  // pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
+  // String formattedDate =
+  // DateFormat('yyyy-MM-dd').format(pickedDate);
+  // print(
+  // formattedDate); //formatted date output using intl package =>  2021-03-16
+  // setState(() {
+  // dateInput.text =
+  // formattedDate; //set output date to TextField value.
+  // });
+  // } else {}
   var dateController;
 
   get dateInput => null;
