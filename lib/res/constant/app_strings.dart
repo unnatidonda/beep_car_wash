2ee4f44bf9c718af3635acd9c6c30d8b03b6a6dc
Unnati Donda <134287519+unnatidonda@users.noreply.github.com>
2023-08-10 18:51:38 +0530

    images

--- lib/res/constant/app_strings.dart
+++ lib/res/constant/app_strings.dart
@@ -7,5 +7,7 @@ class AppStrings {
   static const String continuebeep = "Continue";
   static const String CreateyourAccount = "Create your Account";
   static const String facebook = "Facebook";
+  static const String enterCode = "Enter code to verify number";
+  static const String verification = "We have sent a verification code on your phone number \n+012345443243 Please enter the four number of \nverification code below";
   static const String apple = "apple ID";
 }
