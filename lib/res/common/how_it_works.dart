3fabab7c6c2bf1d91ed710db9567177ec9d17f92
Unnati Donda <134287519+unnatidonda@users.noreply.github.com>
2023-09-10 14:59:02 +0530

    common

--- lib/res/common/how_it_works.dart
+++ lib/res/common/how_it_works.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
 
+import '../constant/app_assets.dart';
+import '../constant/app_colors.dart';
+import '../constant/app_strings.dart';
+
 class HowItWorks extends StatelessWidget {
   const HowItWorks({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Size size = MediaQuery.of(context).size;
+    double screenHeight = size.height;
+    double screenWidth = size.width;
+    EdgeInsets devicePadding = MediaQuery.of(context).viewPadding;
+    return Column(
+      children: [
+        Column(
+          children: [
+            Image.asset(
+              AppAssets.scanning,
+              height: screenHeight / 6,
+              width: screenWidth / 3,
+            ),
+            SizedBox(width: screenWidth / 12),
+            Padding(
+              padding: EdgeInsets.only(left: 26),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  textAlign: TextAlign.start,
+                  AppStrings.scanning,
+                  style: TextStyle(
+                    color: AppColors.lightBlackColor,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                SizedBox(width: screenWidth / 20),
+                Image.asset(
+                  AppAssets.circleCheck,
+                  width: screenWidth / 12,
+                  height: screenHeight / 17,
+                ),
+                // Text(AppS)
+              ],
+            )
+          ],
+        ),
+      ],
+    );
   }
 }
